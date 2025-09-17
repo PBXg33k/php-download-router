a7aab67120438a02e27a8ddc823112d523788bed
PBX_g33k <development@oguzhanuysal.eu>
2025-09-17 22:18:31 +0200

    Update example.spec.js

--- e2e/tests/example.spec.js
+++ e2e/tests/example.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('homepage', async ({ page }) => {
   await page.goto('https://localhost/');
-  await expect(page).toHaveTitle('Welcome to API Platform');
+  await expect(page).toHaveTitle('Welcome to API Platform!');
 });
 
 test('swagger', async ({ page }) => {
